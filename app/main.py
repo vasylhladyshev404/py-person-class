7039bae9912f4c3895e89cad03f625b353db04ea
vasylhladyshev404 <vasyl@nextgear.nl>
2025-07-14 21:32:46 +0200

    2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
+from typing import List, Dict
+
+
 class Person:
-    # write your code here
-    pass
+    people: Dict[str, "Person"] = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_data: List[Dict]) -> List[Person]:
+    person_list = []
+    for data in people_data:
+        person = Person(name=data["name"], age=data["age"])
+        person_list.append(person)
+
+    for data in people_data:
+        person = Person.people[data["name"]]
+        if "wife" in data and data["wife"] is not None:
+            person.wife = Person.people[data["wife"]]
+        if "husband" in data and data["husband"] is not None:
+            person.husband = Person.people[data["husband"]]
+    return person_list
